c2e5674919d2fb3f31c3d07700107c47abf7b808
Paul Bowden <paulbowden673@gmail.com>
2022-02-02 15:22:29 +0000

    fixed profile page

--- checkout/views.py
+++ checkout/views.py
@@ -2,12 +2,11 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpR
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
-
 from .forms import OrderForm
 from .models import Order, OrderLineItem
 from products.models import Product
-# from profiles.forms import UserProfileForm
-#from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 from bag.contexts import bag_contents
 
 import stripe
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,5 @@
+from django.urls import path
+from . import views
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 
