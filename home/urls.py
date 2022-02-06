f8706f6d66bfa81b7b9fdd4eb463573ce1abd190
Paul Bowden <paulbowden673@gmail.com>
2022-02-06 10:19:50 +0000

    added about page

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
+from home.views import AboutView  # , LoseweightView, GainweightView, EnduranceView, LivehealthyView, ContactView
 from . import views
-# from home.views import AboutView, LoseweightView, GainweightView, EnduranceView, LivehealthyView, ContactView
 
 urlpatterns = [
     path('', views.index, name='home'),
-    # path('about/', AboutView.as_view(), name='about'),
+    path('about/', AboutView.as_view(), name='about'),
     # path('lose-weight/', LoseweightView.as_view(), name='lose-weight'),
     # path('gain-weight/', GainweightView.as_view(), name='gain-weight'),
     # path('endurance/', EnduranceView.as_view(), name='endurance'),
