c4b480af75b60ec2f9d565088abf7253ccf4e192
Paul Bowden <paulbowden673@gmail.com>
2022-02-06 11:58:38 +0000

    added endurance html

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from home.views import AboutView, ContactView, LivehealthyView, GainweightView, LoseweightView
-# , EnduranceView,
+from home.views import AboutView, ContactView, LivehealthyView, GainweightView, LoseweightView, EnduranceView
 from . import views
 
 urlpatterns = [
@@ -8,7 +7,7 @@ urlpatterns = [
     path('about/', AboutView.as_view(), name='about'),
     path('lose-weight/', LoseweightView.as_view(), name='lose-weight'),
     path('gain-weight/', GainweightView.as_view(), name='gain-weight'),
-    # path('endurance/', EnduranceView.as_view(), name='endurance'),
+    path('endurance/', EnduranceView.as_view(), name='endurance'),
     path('live-healthy/', LivehealthyView.as_view(), name='live-healthy'),
     path('contact/', ContactView.as_view(), name='contact'),
 ]
