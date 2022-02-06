0b998e6a0f15a1855ddf1745a678ea3235c356c3
Paul Bowden <paulbowden673@gmail.com>
2022-02-06 14:03:48 +0000

    added AWS

--- maximum_effort/settings.py
+++ maximum_effort/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'maximum_effort.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
