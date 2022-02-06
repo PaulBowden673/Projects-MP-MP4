038c14530387fbb701cffeba567a5bdbe1e7273b
Paul Bowden <paulbowden673@gmail.com>
2022-02-06 11:49:18 +0000

    added lose weight html

--- home/urls.py
+++ home/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from home.views import AboutView, ContactView,LivehealthyView, GainweightView
-# , LoseweightView, EnduranceView, 
+from home.views import AboutView, ContactView, LivehealthyView, GainweightView, LoseweightView
+# , EnduranceView,
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', AboutView.as_view(), name='about'),
-    # path('lose-weight/', LoseweightView.as_view(), name='lose-weight'),
+    path('lose-weight/', LoseweightView.as_view(), name='lose-weight'),
     path('gain-weight/', GainweightView.as_view(), name='gain-weight'),
     # path('endurance/', EnduranceView.as_view(), name='endurance'),
     path('live-healthy/', LivehealthyView.as_view(), name='live-healthy'),
