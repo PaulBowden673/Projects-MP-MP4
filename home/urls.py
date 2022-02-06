c5adaf0377fe8815e1db9a620d64a2c928d6e0fc
Paul Bowden <paulbowden673@gmail.com>
2022-02-06 11:35:45 +0000

    added gain-weight page

--- home/urls.py
+++ home/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from home.views import AboutView, ContactView,LivehealthyView
-# , LoseweightView, GainweightView, EnduranceView, 
+from home.views import AboutView, ContactView,LivehealthyView, GainweightView
+# , LoseweightView, EnduranceView, 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
     path('about/', AboutView.as_view(), name='about'),
     # path('lose-weight/', LoseweightView.as_view(), name='lose-weight'),
-    # path('gain-weight/', GainweightView.as_view(), name='gain-weight'),
+    path('gain-weight/', GainweightView.as_view(), name='gain-weight'),
     # path('endurance/', EnduranceView.as_view(), name='endurance'),
     path('live-healthy/', LivehealthyView.as_view(), name='live-healthy'),
     path('contact/', ContactView.as_view(), name='contact'),
