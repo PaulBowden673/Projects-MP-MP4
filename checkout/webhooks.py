6dab77ed011c4eee58a391af5296bf5ce18045ef
Paul Bowden <paulbowden673@gmail.com>
2022-02-08 14:30:54 +0000

    fixed Configure the DEFAULT_AUTO_FIELD

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-import stripe
+
 from checkout.webhook_handler import StripeWH_Handler
 
+import stripe
 
 @require_POST
 @csrf_exempt
@@ -21,7 +22,8 @@ def webhook(request):
 
     try:
         event = stripe.Webhook.construct_event(
-            payload, sig_header, wh_secret)
+        payload, sig_header, wh_secret
+        )
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
@@ -30,6 +32,7 @@ def webhook(request):
         return HttpResponse(status=400)
     except Exception as e:
         return HttpResponse(content=e, status=400)
+
     # Set up a webhook handler
     handler = StripeWH_Handler(request)
 
