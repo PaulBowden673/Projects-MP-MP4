e7564fe18590dfa511bb968ad495a9eefea13c6d
Paul Bowden <paulbowden673@gmail.com>
2022-02-08 14:33:40 +0000

    fixed Configure the DEFAULT_AUTO_FIELD

--- profiles/apps.py
+++ profiles/apps.py
@@ -4,3 +4,13 @@ from django.apps import AppConfig
 class ProfilesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'profiles'
+
+
+class AccountConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'account'
+
+
+class SocialAccountConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'social_account'
