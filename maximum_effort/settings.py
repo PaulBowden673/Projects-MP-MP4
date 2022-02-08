b1bbe658ad663845185e343fb4f54e5e416c0e39
Paul Bowden <paulbowden673@gmail.com>
2022-02-08 15:38:46 +0000

    reviwed host again

--- maximum_effort/settings.py
+++ maximum_effort/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'lse46fz7iv9yr4u#(ge0(is_c09fq=ai6at@lsej78_q6v-8l')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNIN
 # G: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
